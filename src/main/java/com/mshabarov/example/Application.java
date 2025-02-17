7c0304fbe2f321b2b33be194201244592cafdc76
Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>
2025-02-17 13:06:19 +0200

    Create Application.java

---
+++ src/main/java/com/mshabarov/example/Application.java
@@ -0,0 +1,13 @@
+package com.mshabarov.example;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
